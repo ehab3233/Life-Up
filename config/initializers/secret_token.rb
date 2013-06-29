ff5f2e295a753ed8ccdb19fd322aee3d50179fa3
Ehab Herz <ehab.herz@hotmail.com>
2013-06-29 16:43:25 +1000

    created a rails application and added status updates

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lifeup::Application.config.secret_token = '531a99903ace2bdb5f0af3552be72730efab1e61d8471eb575dfb1624e9da9f4f97df2c0d49cdbb3a9f366607338581bb94a4d7fc5360732302714dab0162809'
